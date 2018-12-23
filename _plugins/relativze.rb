ed70714540ba46114549ab099275a4c56507518d
Max Rydahl Andersen <max.andersen@gmail.com>
2018-12-24 00:28:55 +0100

    makes things actually relative

--- _plugins/relativze.rb
+++ _plugins/relativze.rb
@@ -2,10 +2,16 @@ require 'pathname'
 
 module Jekyll
   module UrlRelativizer
-    def relativize_url(url)
-      pageUrl = @context.registers[:page]["url"]
-      pageDir = Pathname(pageUrl).parent
-      Pathname(url).relative_path_from(pageDir).to_s
+    ## this overrides relative_url default filter to actually generate relative urls
+    ## based on github.com/jekyll/jekyll/pull/6362/ (and with some bugfixes from comments)
+    def relative_url(input)
+      return if input.nil?
+      input    = ensure_leading_slash(input)
+      page_url = @context.registers[:page]["url"]
+      page_dir = Pathname(page_url).parent
+
+      target   = page_url.end_with?("/") ? Pathname(page_url) : page_dir
+      Pathname(input).relative_path_from(target).to_s
     end
   end
 end
